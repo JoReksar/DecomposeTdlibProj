2827734e7db8df4a1b8dd564ccbecf0415a1e5ac
Nikita Kokin <nikita.kokin@notissimus.com>
2025-07-22 07:54:34 +0300

    Правка для запуска iOS

--- composeApp/src/iosMain/kotlin/com/joreksar/decomposetdlibproject/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/joreksar/decomposetdlibproject/MainViewController.kt
@@ -1,5 +1,11 @@
 package com.joreksar.decomposetdlibproject
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.joreksar.decomposetdlibproject.navigation.RootComponent
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val root = RootComponent(DefaultComponentContext(LifecycleRegistry()))
+    App(root)
+}
