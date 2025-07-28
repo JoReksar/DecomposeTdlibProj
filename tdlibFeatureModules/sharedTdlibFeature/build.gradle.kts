92f9d442713d471b542479a1edfc735581c2764a
Nikita Kokin <nikita.kokin@notissimus.com>
2025-07-28 04:28:15 +0300

    Добавил нативную iOS библиотеку tdlib

--- tdlibFeatureModules/sharedTdlibFeature/build.gradle.kts
+++ tdlibFeatureModules/sharedTdlibFeature/build.gradle.kts
@@ -8,6 +8,30 @@ plugins {
 
 kotlin {
 
+    // For iOS targets, this is also where you should
+    // configure native binary output. For more information, see:
+    // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
+
+    // A step-by-step guide on how to include this library in an XCode
+    // project can be found here:
+    // https://developer.android.com/kotlin/multiplatform/migrate
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget>().configureEach {
+        binaries.framework {
+            baseName = "sharedTdlibFeatureKit"
+        }
+
+        compilations["main"].cinterops {
+            create("TDLib") {
+                definitionFile = file("src/iosMain/cinterop/tdlib.def")
+                includeDirs("src/iosMain/cinterop/headers")
+            }
+        }
+    }
+
     // Target declarations - add or remove as needed below. These define
     // which platforms this KMP module supports.
     // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
@@ -23,33 +47,6 @@ kotlin {
             sourceSetTreeName = "test"
         }.configure {
             instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        }
-    }
-
-    // For iOS targets, this is also where you should
-    // configure native binary output. For more information, see:
-    // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
-
-    // A step-by-step guide on how to include this library in an XCode
-    // project can be found here:
-    // https://developer.android.com/kotlin/multiplatform/migrate
-    val xcfName = "sharedTdlibFeatureKit"
-
-    iosX64 {
-        binaries.framework {
-            baseName = xcfName
-        }
-    }
-
-    iosArm64 {
-        binaries.framework {
-            baseName = xcfName
-        }
-    }
-
-    iosSimulatorArm64 {
-        binaries.framework {
-            baseName = xcfName
         }
     }
 
